7d83bebb700e56de075061600740ea8eeb618a21
zhu-to-u <2528487925@qq.com>
2024-04-01 22:31:08 +0800

    注册节点-借助一致性哈希算法选择节点并实现HTTP客户端-与远程节点服务端通信

---
+++ geecache/http.go
@@ -0,0 +1,129 @@
+package geecache
+
+import (
+	"GeeCache/consistenthash"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+)
+
+const (
+	defaultBasePath = "/_geecache/"
+	defaultReplicas = 50
+)
+
+type HTTPPool struct {
+	self        string //用来记录自己的地址
+	basePath    string //作为节点间通讯地址的前缀
+	mu          sync.Mutex
+	peers       *consistenthash.Map    //选择具体的key节点
+	httpGetters map[string]*httpGetter //映射远程节点和对应的 httpGetter
+}
+
+type httpGetter struct {
+	baseURL string //即将访问的远程节点的地址
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+// Log info with server name
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+// w用于向客户端发送http响应，可以设置响应的状态码、响应头、响应体
+// r代表客户端发起的HTTP请求
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//是否以特定的前缀开头
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool sreving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	//路径切割
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
+
+// 实例化一致性哈希算法，添加了传入的节点，为每一个节点创建了HTTP客户端
+// 参数为构建算法的节点的地址
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _PeerPicker = (*HTTPPool)(nil)
