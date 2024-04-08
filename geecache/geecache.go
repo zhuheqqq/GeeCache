dcb229ab903a98fb2b7b19fe1875bfb109895ac2
zhu-to-u <2528487925@qq.com>
2024-04-08 23:00:38 +0800

    使用protobuf通信

--- geecache/geecache.go
+++ geecache/geecache.go
@@ -1,6 +1,7 @@
 package geecache
 
 import (
+	pb "GeeCache/geecachepb"
 	"GeeCache/singleflight"
 	"fmt"
 	"log"
@@ -56,11 +57,16 @@ func (g *Group) load(key string) (value ByteView, err error) {
 }
 
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
-	bytes, err := peer.Get(g.name, key)
+	req := &pb.Request{
+		Group: g.name,
+		Key:   key,
+	}
+	res := &pb.Response{}
+	err := peer.Get(req, res)
 	if err != nil {
 		return ByteView{}, err
 	}
-	return ByteView{b: bytes}, nil
+	return ByteView{b: res.Value}, nil
 }
 
 func (f GetterFunc) Get(key string) ([]byte, error) {
--- geecache/peers.go
+++ geecache/peers.go
@@ -1,9 +1,11 @@
 package geecache
 
+import pb "GeeCache/geecachepb"
+
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
 }
 
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
